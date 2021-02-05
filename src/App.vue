4f5cc2be3ea954f471b1fad68f2d6ba7f15d23f1
NaemHaque <naeemhaque248@gmail.com>
2021-02-05 17:03:03 +0600

    init

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,38 @@
 <template>
-  <v-card class="overflow-hidden">
-    <v-app-bar
-      :collapse="!collapseOnScroll"
-      :collapse-on-scroll="collapseOnScroll"
-      absolute
-      color="deep-purple accent-4"
-      dark
-      scroll-target="#scrolling-techniques-6"
-    >
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
-
-      <v-toolbar-title>Collapsing Bar</v-toolbar-title>
-
-      <v-spacer></v-spacer>
-
-      <v-checkbox
-        v-model="collapseOnScroll"
-        color="white"
-        hide-details
-      ></v-checkbox>
-    </v-app-bar>
-    <v-sheet
-      id="scrolling-techniques-6"
-      class="overflow-y-auto"
-      max-height="600"
-    >
-      <v-container style="height: 1000px;"></v-container>
-    </v-sheet>
-  </v-card>
+	<v-card width="400" class="mx-auto mt-5">
+		<v-card-title>
+			<h1 class="display-1">Login</h1>
+		</v-card-title>
+		<v-card-text>
+			<v-form>
+				<v-text-field label="Username" prepend-icon="mdi-account-circle" />
+				<v-text-field
+					:type="showPassword ? 'text' : 'password'"
+					label="Password"
+					prepend-icon="mdi-lock"
+					:append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+					@click:append="showPassword = !showPassword"
+				/>
+			</v-form>
+		</v-card-text>
+		<v-divider></v-divider>
+		<v-card-actions>
+			<v-btn color="success">Register</v-btn>
+			<v-spacer></v-spacer>
+			<v-btn color="info">Login</v-btn>
+		</v-card-actions>
+	</v-card>
 </template>
+
+<script>
+export default {
+	name: 'LoginPage',
+	data() {
+		return {
+			showPassword: false
+		}
+	}
+}
+</script>
+
+<style></style>
