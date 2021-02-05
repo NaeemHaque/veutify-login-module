47bb7f9592639beffcc92e81455d356d9c17208a
NaemHaque <naeemhaque248@gmail.com>
2021-02-05 16:50:02 +0600

    init

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+
+
+
+
